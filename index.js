d0796c1ee54aa42fd516d543bd4e8c49efa8310f
Swati Mukherjee <swati@cronj.com>
2017-02-07 16:47:43 +0530

    working-search,pagination,range using node.js

--- index.js
+++ index.js
@@ -1,15 +1,24 @@
-var http = require('http');
+var http=require("http");
 var fs = require('fs');
+var path = require('path');
+const url_req = require('url') ;
+var lodash = require('lodash');
 var data = JSON.parse(fs.readFileSync('details.json', 'utf8'));
+//******************************seperate from the url query************************************************
+function getQueryVariable(variable) {
+    var vars = variable.replace("&","=").split("=");
+    return vars ; 
+}
 http.createServer(function(request,response){
-	 var url = request.url;
-  console.log(url);
+  var url = url_req.parse(request.url).pathname;
+  //console.log();
   if(url == '/')
   {
 
 		fs.readFile(__dirname + '/index.html','utf8', function (err, data) {
 		if (err) console.log(err);
-		else{response.writeHead(200, {'Content-Type': 'text/html'});
+		else
+			{response.writeHead(200, {'Content-Type': 'text/html'});
 				response.write(data);
 				response.end();}
     });
@@ -57,12 +66,70 @@ http.createServer(function(request,response){
     });
 
   }
-   
+   //******************************get /data from the url and process its parameters************************************************
+
   else if(url == '/data')
   {
-    response.setHeader('Content-Type', 'application/json');
- 	response.write(JSON.stringify(data));
-  	response.end();
+  	
+    if(url_req.parse(request.url).query!=null){
+		var query = url_req.parse(request.url).query;
+
+		var getSeperated = getQueryVariable(query);
+
+		fs.readFile(__dirname + '/details.json','utf8', function (err, data) {	
+		
+			if (err) 
+				console.log(err);
+			else
+			{
+				//console.log("type-data"+typeof(data));
+				//console.log("type-parse"+typeof(JSON.parse(data)));
+				//console.log("type-items"+typeof(JSON.parse(data).items));
+
+
+				var items = JSON.parse(data).items;
+				var len = items.length ; 
+				var nameOfItems = [];
+				for(var i =0 ; i<len ; i++)
+				{
+					if(lodash.includes(items[i].name,getSeperated[1]))
+					{
+						nameOfItems.push(items[i]);
+					}
+				}
+				var page = getSeperated[3];
+				 
+				var items =  [] ;
+				////console.log(nameOfItems);
+				var begin = (page-1)*10 ;
+				var end = begin +10 ;
+				for(var i=begin;i<end && i<nameOfItems.length;i++){
+						items.push(nameOfItems[i]);
+				} 
+				////console.log(items);
+				response.writeHead(200, {'Content-Type': 'text/plain'});
+				response.write(JSON.stringify(items));
+				response.end(); 
+			}	
+		});
+
+	}
+	else 
+	{
+	//console.log("inside data");
+	fs.readFile(__dirname + '/details.json','utf8', function (err, data) {	
+	//console.log("here get projects")
+	if (err) 
+		console.log(err);
+	else{
+
+		response.writeHead(200, {'Content-Type': 'text/plain'});
+		response.write(data);
+		response.end();
+	}	
+});
+}
+
   }
   
 }).listen(8080);
--- main.js
+++ main.js
@@ -3,27 +3,27 @@ $(document).ready(function()
 	var data1; 
 
 	$('#pagntn').twbsPagination({
-        totalPages: 50,
+        totalPages: 20,
         visiblePages: 5,
-        itemOnPage: 10,
 
         onPageClick: function (event, page)
          {
 		    var name=$("#searchName").val();
-		    if(name==null||name=="")
-		    	name="yahoo";
-		    $.getJSON( "http://127.0.0.1:8080/data", function( data ) 
+		    	//name="yahoo";
+
+		    $.getJSON("http://localhost:8080/data?name="+name+"&page="+page, function(data) 
 					{   
 						data1=data;
+						//console.log("data:"+data1)
 						$(".name").html("");
 						$("#search").val("");
 						
-					    console.log( "JSON Data: " + data.items[0].name);
+					   // //console.log( "JSON Data: " + data.items[0].name);
 					    var i=0;var count=0;
-						for (i in data.items) 
+						for (i in data) 
 						{
 								
-							$('.name').append(`<p id="idLink">&raquo;<a href="`+data.items[i].html_url+`" target="_blank">`+data.items[i].full_name+` </a><br>`+data.items[i].language+`&nbsp; &#124; `+data.items[i].description+`</p>`);
+							$('.name').append(`<p id="idLink">&raquo;<a href="`+data[i].html_url+`" target="_blank">`+data[i].full_name+` </a><br>`+data[i].language+`&nbsp; &#124; `+data[i].description+`</p>`);
 				    		count++;
 				    	}
 				    	if(count!=0) 
@@ -39,12 +39,12 @@ $(document).ready(function()
 
 			 	$("#lr1").text(rangeVal);
 				$(".name").html("");
-				for (i in data1.items) 
+				for (i in data1) 
 				{
-					if (data1.items[i].stargazers_count>=rangeVal) 
+					if (data1[i].stargazers_count<=rangeVal) 
 					{
-						$('.name').append(`<p id="idLink">&raquo;<a href="`+data1.items[i].html_url+`" target="_blank">`+data1.items[i].full_name+` </a><br>`+data1.items[i].language+`&nbsp; &#124; `+data1.items[i].description+`</p>`);
-		    			count++;
+						$('.name').append(`<p id="idLink">&raquo;<a href="`+data1[i].html_url+`" target="_blank">`+data1[i].full_name+` </a><br>`+data1[i].language+`&nbsp; &#124; `+data1[i].description+`</p>`);
+		 		count++;
 		    		}
 		    	}
 			    	if(count!=0) 
@@ -56,28 +56,27 @@ $(document).ready(function()
 
 
 
-		    	$('#search').on('input',function(e)
-		    	{
-				    	
-					$(".name").html("");
-					var i=0;
-					var var2=$("#search").val().toLowerCase();
-					for (i in data1.items) 
-					{
-						var var1=(data1.items[i].language).toLowerCase();
-						if ((var1==var2)&&(data1.items[i].stargazers_count>=$("#starR").val()))
-						{				
-							count++;
-							$('.name').append(`<p id="idLink">&raquo;<a href="`+data1.items[i].html_url+`" target="_blank">`+data1.items[i].full_name+` </a><br>`+data1.items[i].language+`&nbsp; &#124; `+data1.items[i].description+`</p>`);
-						}			
-					}
-					if(count!=0) 
-						$("#display").text(count+" out of 10 records"); 
-					else 
-						$("#display").text("Sorry! No Records found.");
+		  //   	$('#search').on('input',function(e)
+		  //   	{
+								    	
+				// $(".name").html(""); 
+				// var i=0;
+				// var var2=$("#search").val().toLowerCase(); 
+				// for (i in data1)  
+				// 	{     
+				// 		var var1=(data1[i].language).toLowerCase();     
+				// 		if ((var1==var2)&&(data1[i].stargazers_count>=$("#starR").val()))
+				// 			{    count++;
+				// 			$('.name').append(`<p id="idLink">&raquo;<a href="`+data1[i].html_url+`"target="_blank">`+data1[i].full_name+`</a><br>`+data1[i].language+`&nbsp; &#124;`+data1[i].description+`</p>`);    
+				// 			 }
+				// 			} 
+				// 			if(count!=0)
+				// 			$("#display").text(count+" out of 10 records");  
+				// 			else
+				// 			$("#display").text("Sorry! No Records found.");
 
-				});
-				
+				// 							});
+				//search by language
 				$("#search").keypress(function (e) 
 				{
 					i=0; count=0;
@@ -85,31 +84,36 @@ $(document).ready(function()
 			    	{
 			    		$(".name").html("");
 			    		if(!$("#search").val())
-			    		{
-			    			for (i in data1.items) 
+			    		{ 
+			    			//console.log("data1 "+data1);
+			    			for (i in data1) 
 								{
 							
-									$('.name').append(`<p id="idLink">&raquo;<a href="`+data1.items[i].html_url+`" target="_blank">`+data1.items[i].full_name+` </a><br>`+data1.items[i].language+`&nbsp; &#124; `+data1.items[i].description+`</p>`);
-			    					count++;
+									$('.name').append(`<p id="idLink">&raquo;<a href="`+data1[i].html_url+`" target="_blank">`+data1[i].full_name+` </a><br>`+data1[i].language+`&nbsp; &#124; `+data1[i].description+`</p>`);
+			    			
 			    				}	
-
+			    					count++;
 			    		}
-
 
 			    		else
 						{
 							$(".name").html("");
-							var i=0;
+
 							var var2=$("#search").val().toLowerCase();
-									for (i in data1.items) 
-									{
-										var var1=(data1.items[i].language).toLowerCase();
-										if ((var1==var2)&&(data1.items[i].stargazers_count>=$("#starR").val()))
-										{				
-											count++;
-											$('.name').append(`<p id="idLink">&raquo;<a href="`+data1.items[i].html_url+`" target="_blank">`+data1.items[i].full_name+` </a><br>`+data1.items[i].language+`&nbsp; &#124; `+data1.items[i].description+`</p>`);
-										}			
-									}
+							
+							for (i in data1) 
+							{
+								var var1=(data1[i].language).toLowerCase();
+								
+								//if ((var1==var2)&&(data1[i].stargazers_count>=$("#starR").val()))
+								//console.log(var1+" "+var2);
+								if (var1==var2)
+								{				
+									count++;
+									//console.log("matchi8ng");
+									$('.name').append(`<p id="idLink">&raquo;<a href="`+data1[i].html_url+`" target="_blank">`+data1[i].full_name+` </a><br>`+data1[i].language+`&nbsp; &#124; `+data1[i].description+`</p>`);
+								}			
+							}
 					   	 }
 					}
 
@@ -122,34 +126,36 @@ $(document).ready(function()
 				$("#searchName").keypress(function (e) 
 				{
 					var name=$("#searchName").val();
-					if(name==null||name=="")
-		    		name="yahoo";
+
+					// if(name==null||name=="")
+		   			//  name="yahoo";
 		    		if (e.which == 13) 
 		    		{
-		    			$.getJSON( "http://127.0.0.1:8080/data", function( data ) 
+		    			$.getJSON("http://localhost:8080/data?name="+name+"&page=1", function( data ) 
 						{   
-							data1=data;
-							var totalPages = Math.ceil(data.total_count/10);
-							//
-							var defaultOpts = {
-											        totalPages: 20
-											    };
-					            var currentPage = 1;
-					             $("#pagntn").twbsPagination('destroy');
-					            $("#pagntn").twbsPagination($.extend({}, {
-   					              	totalPages: totalPages,
-					                 startPage: currentPage,
-					            }));
-					        //
+							
+							// var totalPages = Math.ceil(data.total_count/10);
+							// var defaultOpts = 
+							//	{
+							// 		totalPages:20
+							// 		};
+						  	// var currentPage = 1;
+						  	// $("#pagntn").twbsPagination('destroy');
+						  	// $("#pagntn").twbsPagination($.extend({}, 
+						  	//	{
+	   						// 		totalPages: totalPages,
+						  	//  	startPage: currentPage,
+						  	//      }));
+						  	//console.log("name:"+name);
+						  	data1 = data ;
 							$(".name").html("");
-							$("#search").val("");
-				
-						    console.log( "JSON Data: " + data.items[0].name);
+							//$("#search").val("");
+							//console.log("DATA:"+data1);
+						    //console.log( "JSON Data: " + data[0].name);
 						    var i=0;var count=0;
-							for (i in data.items) 
+							for (i in data) 
 							{
-									
-								$('.name').append(`<p id="idLink">&raquo;<a href="`+data.items[i].html_url+`" target="_blank">`+data.items[i].full_name+` </a><br>`+data.items[i].language+`&nbsp; &#124; `+data.items[i].description+`</p>`);
+								$('.name').append(`<p id="idLink">&raquo;<a href="`+data[i].html_url+`" target="_blank">`+data[i].full_name+` </a><br>`+data[i].language+`&nbsp; &#124; `+data[i].description+`</p>`);
 					    		count++;
 					    	}
 					    	if(count!=0) 
@@ -161,7 +167,7 @@ $(document).ready(function()
 		    		}
 
 		    		else 
-					    		$("#display").text("Sorry! No Records found.");
+					    $("#display").text("Sorry! No Records found.");
 				});
 
 		}
