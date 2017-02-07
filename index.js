ec1a69aa6ee85b50d72ae9804762f40f31a9c187
Swati Mukherjee <swati@cronj.com>
2017-02-07 18:11:23 +0530

    commented+modularity_improved

--- index.js
+++ index.js
@@ -4,13 +4,13 @@ var path = require('path');
 const url_req = require('url') ;
 var lodash = require('lodash');
 var data = JSON.parse(fs.readFileSync('details.json', 'utf8'));
-//******************************seperate from the url query************************************************
+//******************************seperate from the url query**********************************
 function getQueryVariable(variable) {
     var vars = variable.replace("&","=").split("=");
     return vars ; 
 }
 
-
+//******************************distribute url accordingly************************************
 http.createServer(function(request,response){
 	function getUrl(name,type)
 {
@@ -22,92 +22,55 @@ http.createServer(function(request,response){
 				response.writeHead(200, {'Content-Type': type});
 				response.write(data);
 				response.end();
-			}
-});}
-
-
-  var url = url_req.parse(request.url).pathname;
-  //console.log();
+			}});}
+//******************************ask url accordingly*******************************************
   if(url == '/')
   getUrl('index.html','text/html');
-	  
   else   if(url == '/main.js')
   	    getUrl('main.js','text/javascript');
-    
- 
   else   if(url == '/style.css')
    		  	    getUrl('style.css','text/css');
-  
   else   if(url == '/gitimage.png')
   					console.log("load image");
-   
   else   if(url == '/jquery.twbsPagination.js')
            		  	    getUrl('jquery.twbsPagination.js','text/js');
-
-   //******************************get /data from the url and process its parameters************************************************
-
+  
+//******************************get /data from the url and process its parameters****************
   else if(url == '/data')
   {
-  	
-    if(url_req.parse(request.url).query!=null){
+  	    if(url_req.parse(request.url).query!=null){
 		var query = url_req.parse(request.url).query;
-
 		var getSeperated = getQueryVariable(query);
-
 		fs.readFile(__dirname + '/details.json','utf8', function (err, data) {	
-		
 			if (err) 
 				console.log(err);
 			else
 			{
-				//console.log("type-data"+typeof(data));
-				//console.log("type-parse"+typeof(JSON.parse(data)));
-				//console.log("type-items"+typeof(JSON.parse(data).items));
-
-
 				var items = JSON.parse(data).items;
 				var len = items.length ; 
 				var nameOfItems = [];
 				for(var i =0 ; i<len ; i++)
 				{
 					if(lodash.includes(items[i].name,getSeperated[1]))
-					{
 						nameOfItems.push(items[i]);
-					}
 				}
 				var page = getSeperated[3];
-				 
 				var items =  [] ;
-				////console.log(nameOfItems);
 				var begin = (page-1)*10 ;
 				var end = begin +10 ;
-				for(var i=begin;i<end && i<nameOfItems.length;i++){
-						items.push(nameOfItems[i]);
-				} 
-				////console.log(items);
+				for(var i=begin;i<end && i<nameOfItems.length;i++)
+					items.push(nameOfItems[i]);
 				response.writeHead(200, {'Content-Type': 'text/plain'});
 				response.write(JSON.stringify(items));
 				response.end(); 
-			}	
-		});
-
-	}
+			}});}
 	else 
 	{
-	//console.log("inside data");
 	fs.readFile(__dirname + '/details.json','utf8', function (err, data) {	
-	//console.log("here get projects")
 	if (err) 
 		console.log(err);
 	else{
-
 		response.writeHead(200, {'Content-Type': 'text/plain'});
 		response.write(data);
 		response.end();
-	}	
-});
-}
-
-  }
-  
-}).listen(8080);
+	}});}}}).listen(8080);
--- main.js
+++ main.js
@@ -1,28 +1,108 @@
 $(document).ready(function()
 {
-	var data1; 
+var data1; 
+$('#pagntn').twbsPagination({
+    totalPages: 20,
+    visiblePages: 5,
+    onPageClick: function (event, page)
+     {
+     	//******************************on load of page**********************************
+	    var name=$("#searchName").val();
+	    $.getJSON("http://localhost:8080/data?name="+name+"&page="+page, function(data) 
+				{   
+					data1=data;
+					$(".name").html("");
+					$("#search").val("");
+				    var i=0;var count=0;
+					for (i in data) 
+					{	
+						$('.name').append(`<p id="idLink">&raquo;<a href="`+data[i].html_url+`" target="_blank">`+data[i].full_name+` </a><br>`+data[i].language+`&nbsp; &#124; `+data[i].description+`</p>`);
+			    		count++;
+			    	}
+			    	if(count!=0) 
+			    		$("#display").text(count+" out of 10 records"); 
+			    	else 
+			    		$("#display").text("Sorry! No Records found.");
 
-	$('#pagntn').twbsPagination({
-        totalPages: 20,
-        visiblePages: 5,
+			    });
+	 	//******************************code on range**********************************
+		$("[type=range]").change(function()
+		{
+		 	i=0;var count=0;
+		 	var rangeVal=$("#starR").val();
 
-        onPageClick: function (event, page)
-         {
-		    var name=$("#searchName").val();
-		    	//name="yahoo";
+		 	$("#lr1").text(rangeVal);
+			$(".name").html("");
+			for (i in data1) 
+			{
+				if (data1[i].stargazers_count<=rangeVal) 
+				{
+					$('.name').append(`<p id="idLink">&raquo;<a href="`+data1[i].html_url+`" target="_blank">`+data1[i].full_name+` </a><br>`+data1[i].language+`&nbsp; &#124; `+data1[i].description+`</p>`);
+	 		count++;
+	    		}
+	    	}
+		    	if(count!=0) 
+		    		$("#display").text(count+" out of 10 records"); 
+		    	else 
+		    		$("#display").text("Sorry! No Records found.");
+		    
+		});
 
-		    $.getJSON("http://localhost:8080/data?name="+name+"&page="+page, function(data) 
-					{   
-						data1=data;
-						//console.log("data:"+data1)
-						$(".name").html("");
-						$("#search").val("");
+		//******************************search by language**********************************
+			$("#search").keypress(function (e) 
+			{
+				i=0; count=0;
+		    	if (e.which == 13) 
+		    	{
+		    		$(".name").html("");
+		    		if(!$("#search").val())
+		    		{ 
+		    			//console.log("data1 "+data1);
+		    			for (i in data1) 
+							{
 						
-					   // //console.log( "JSON Data: " + data.items[0].name);
-					    var i=0;var count=0;
+								$('.name').append(`<p id="idLink">&raquo;<a href="`+data1[i].html_url+`" target="_blank">`+data1[i].full_name+` </a><br>`+data1[i].language+`&nbsp; &#124; `+data1[i].description+`</p>`);
+		    			
+		    				}	
+		    					count++;
+		    		}
+
+		    		else
+					{
+						$(".name").html("");
+
+						var var2=$("#search").val().toLowerCase();
+						
+						for (i in data1) 
+						{
+							var var1=(data1[i].language).toLowerCase();
+							if (var1==var2)
+							{				
+								count++;
+								$('.name').append(`<p id="idLink">&raquo;<a href="`+data1[i].html_url+`" target="_blank">`+data1[i].full_name+` </a><br>`+data1[i].language+`&nbsp; &#124; `+data1[i].description+`</p>`);
+							}			
+						}
+				   	 }
+				}
+				if(count!=0) 
+					$("#display").text(count+" out of 10 records"); 
+				else 
+					$("#display").text("Sorry! No Records found.");
+			});
+
+//******************************search by Name**********************************
+			$("#searchName").keypress(function (e) 
+			{
+				var name=$("#searchName").val();
+				if (e.which == 13) 
+	    		{
+	    			$.getJSON("http://localhost:8080/data?name="+name+"&page=1", function( data ) 
+					{   
+					data1 = data ;
+						$(".name").html("");
+						var i=0;var count=0;
 						for (i in data) 
 						{
-								
 							$('.name').append(`<p id="idLink">&raquo;<a href="`+data[i].html_url+`" target="_blank">`+data[i].full_name+` </a><br>`+data[i].language+`&nbsp; &#124; `+data[i].description+`</p>`);
 				    		count++;
 				    	}
@@ -32,147 +112,11 @@ $(document).ready(function()
 				    		$("#display").text("Sorry! No Records found.");
 
 				    }); 
-			$("[type=range]").change(function()
-			{
-			 	i=0;var count=0;
-			 	var rangeVal=$("#starR").val();
-
-			 	$("#lr1").text(rangeVal);
-				$(".name").html("");
-				for (i in data1) 
-				{
-					if (data1[i].stargazers_count<=rangeVal) 
-					{
-						$('.name').append(`<p id="idLink">&raquo;<a href="`+data1[i].html_url+`" target="_blank">`+data1[i].full_name+` </a><br>`+data1[i].language+`&nbsp; &#124; `+data1[i].description+`</p>`);
-		 		count++;
-		    		}
-		    	}
-			    	if(count!=0) 
-			    		$("#display").text(count+" out of 10 records"); 
-			    	else 
-			    		$("#display").text("Sorry! No Records found.");
-			    
+	    		}
+	    		else 
+				    $("#display").text("Sorry! No Records found.");
 			});
 
+	}
 
-
-		  //   	$('#search').on('input',function(e)
-		  //   	{
-								    	
-				// $(".name").html(""); 
-				// var i=0;
-				// var var2=$("#search").val().toLowerCase(); 
-				// for (i in data1)  
-				// 	{     
-				// 		var var1=(data1[i].language).toLowerCase();     
-				// 		if ((var1==var2)&&(data1[i].stargazers_count>=$("#starR").val()))
-				// 			{    count++;
-				// 			$('.name').append(`<p id="idLink">&raquo;<a href="`+data1[i].html_url+`"target="_blank">`+data1[i].full_name+`</a><br>`+data1[i].language+`&nbsp; &#124;`+data1[i].description+`</p>`);    
-				// 			 }
-				// 			} 
-				// 			if(count!=0)
-				// 			$("#display").text(count+" out of 10 records");  
-				// 			else
-				// 			$("#display").text("Sorry! No Records found.");
-
-				// 							});
-				//search by language
-				$("#search").keypress(function (e) 
-				{
-					i=0; count=0;
-			    	if (e.which == 13) 
-			    	{
-			    		$(".name").html("");
-			    		if(!$("#search").val())
-			    		{ 
-			    			//console.log("data1 "+data1);
-			    			for (i in data1) 
-								{
-							
-									$('.name').append(`<p id="idLink">&raquo;<a href="`+data1[i].html_url+`" target="_blank">`+data1[i].full_name+` </a><br>`+data1[i].language+`&nbsp; &#124; `+data1[i].description+`</p>`);
-			    			
-			    				}	
-			    					count++;
-			    		}
-
-			    		else
-						{
-							$(".name").html("");
-
-							var var2=$("#search").val().toLowerCase();
-							
-							for (i in data1) 
-							{
-								var var1=(data1[i].language).toLowerCase();
-								
-								//if ((var1==var2)&&(data1[i].stargazers_count>=$("#starR").val()))
-								//console.log(var1+" "+var2);
-								if (var1==var2)
-								{				
-									count++;
-									//console.log("matchi8ng");
-									$('.name').append(`<p id="idLink">&raquo;<a href="`+data1[i].html_url+`" target="_blank">`+data1[i].full_name+` </a><br>`+data1[i].language+`&nbsp; &#124; `+data1[i].description+`</p>`);
-								}			
-							}
-					   	 }
-					}
-
-					if(count!=0) 
-						$("#display").text(count+" out of 10 records"); 
-					else 
-						$("#display").text("Sorry! No Records found.");
-				});
-
-				$("#searchName").keypress(function (e) 
-				{
-					var name=$("#searchName").val();
-
-					// if(name==null||name=="")
-		   			//  name="yahoo";
-		    		if (e.which == 13) 
-		    		{
-		    			$.getJSON("http://localhost:8080/data?name="+name+"&page=1", function( data ) 
-						{   
-							
-							// var totalPages = Math.ceil(data.total_count/10);
-							// var defaultOpts = 
-							//	{
-							// 		totalPages:20
-							// 		};
-						  	// var currentPage = 1;
-						  	// $("#pagntn").twbsPagination('destroy');
-						  	// $("#pagntn").twbsPagination($.extend({}, 
-						  	//	{
-	   						// 		totalPages: totalPages,
-						  	//  	startPage: currentPage,
-						  	//      }));
-						  	//console.log("name:"+name);
-						  	data1 = data ;
-							$(".name").html("");
-							//$("#search").val("");
-							//console.log("DATA:"+data1);
-						    //console.log( "JSON Data: " + data[0].name);
-						    var i=0;var count=0;
-							for (i in data) 
-							{
-								$('.name').append(`<p id="idLink">&raquo;<a href="`+data[i].html_url+`" target="_blank">`+data[i].full_name+` </a><br>`+data[i].language+`&nbsp; &#124; `+data[i].description+`</p>`);
-					    		count++;
-					    	}
-					    	if(count!=0) 
-					    		$("#display").text(count+" out of 10 records"); 
-					    	else 
-					    		$("#display").text("Sorry! No Records found.");
-	
-					    }); 
-		    		}
-
-		    		else 
-					    $("#display").text("Sorry! No Records found.");
-				});
-
-		}
-
-	});});
-
-
-		
+});});		
